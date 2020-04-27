a4b7ced511f079cdef17a3ac10c118e0804181c0
Jmy-dev <61315548+Jmy-dev@users.noreply.github.com>
2020-04-27 21:35:59 +0200

    adding profile Schema and profile route and test with post man

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,9 @@
-const express = require("express") ,
-      router = express.Router();
+const express       = require("express") ,
+      mongoose      = require("mongoose"),
+      passport      = require("passport"),
+      User          = require("../../models/User"),
+      Profile       = require("../../models/Profile"),
+      router        = express.Router();
 // @route    GET api/profile/test
 // @desc     Tests profile route
 // @access   public
@@ -8,4 +12,28 @@ router.get("/test" , (req, res)=>{
     msg: "profile works!!"
   })
 })
+
+// @route    GET api/profile/currentUser
+// @desc     get currentUser profile
+// @access   Private
+
+router.get("/" , passport.authenticate('jwt' , {session :false}) , (req, res)=>{
+  const errors = {};
+  Profile.findOne({user: req.user.id})
+  .then(profile =>{
+    if(!profile){
+      errors.noprofile = 'Profile not exists';
+      return res.status(404).json(errors);
+    }
+    res.json(profile);
+  })
+  .catch(err => console.log(err))
+});
+
+
+
+
+
+
+
 module.exports = router;
