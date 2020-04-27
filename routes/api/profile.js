120b606bd5f64a45bffd9cfa29f7981487b4cbb9
Jmy-dev <61315548+Jmy-dev@users.noreply.github.com>
2020-04-27 23:15:54 +0200

    add profile post and eidt route and waiting for validation then will test it

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -13,7 +13,7 @@ router.get("/test" , (req, res)=>{
   })
 })
 
-// @route    GET api/profile/currentUser
+// @route    GET api/profile
 // @desc     get currentUser profile
 // @access   Private
 
@@ -28,6 +28,59 @@ router.get("/" , passport.authenticate('jwt' , {session :false}) , (req, res)=>{
     res.json(profile);
   })
   .catch(err => console.log(err))
+});
+
+
+// @route    POST api/profile
+// @desc     create or eidt currentUser profile
+// @access   Private
+
+router.get("/" , passport.authenticate('jwt' , {session :false}) , (req, res)=>{
+  const profileFields ={};
+  profileFields.user = req.body.id ;
+  if(req.body.handle) profileFields.handle = req.body.handle;
+  if(req.body.company) profileFields.company = req.body.company;
+  if(req.body.website) profileFields.website = req.body.website;
+  if(req.body.bio) profileFields.bio = req.body.bio;
+  if(req.body.location) profileFields.location = req.body.location;
+  if(req.body.status) profileFields.status = req.body.status;
+  if(req.body.githubname) profileFields.githubname = req.body.githubname;
+  // skills
+   if (typeof req.body.skills !== 'undefined'){
+     profileFields.skills = req.body.skills
+   }
+   //social
+   profileFields.social = {};
+   if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+   if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+   if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+   if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+   if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+
+   Profile.findOne({user: req.user.id})
+      .then( profile =>{
+        if(profile){
+          //update
+          Profile.findOneAndId({user : req.user.id} , {$set: profileFields}, {new: true })
+             .then( profile => res.json(profile))
+        }else{
+          //create
+
+          //check for handle
+          Profile.findOne({handle: profileFields.handle})
+            .then(profile =>{
+              if (profile) {
+                errors.handle = 'that handle already exists';
+                return res.status(400).json(errors)
+              }else{
+                //create
+                new Profile(profile).save()
+                then(profile => res.json(profile))
+              }
+            })
+
+        }
+      })
 });
 
 
